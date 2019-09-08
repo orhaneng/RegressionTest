0b8d4679d13f31c69147d17e4a615ce3dd75fbe0
Omer Orhan <omer.orhan@edriving.com>
2019-09-07 21:09:04 -0700

    cc

--- venv/src/regressiontest.py
+++ venv/src/regressiontest.py
@@ -1,35 +1,46 @@
-import os
+
+
+import os, sys
 import socket
 from clear_database import clear_dynamodb
 from telematics_multiprocess import upload_bin_batch_v2
 from get_trip_from_regression import getTripsFromRegressionServer
 import pandas as pd
 from comparision import compareTrips
+import platform
 print("=======REGRESSION TEST==========")
-FOLDER_PATH = "/Users/omerorhan/Documents/EventDetection/regression_server/regressiontest/"
 
-if len(os.listdir(FOLDER_PATH + "build/")) > 0:
-    print("current(under build folder) version will be tested!")
-else:
-    print("can not be continued without build! Put telematics folder under the build directory!")
-    exit()
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if sock.connect_ex(('localhost', 8000)) != 0:
-    print("activate DynamoDB! Please open new command prompt and run code in Readme.txt")
-    response = input("Is DynamoDB activated(Y/N)?")
-    if response.upper() != 'Y' and sock.connect_ex(('localhost', 8000)) != 0:
-        pass
+def test(FOLDER_PATH):
+    if len(os.listdir(FOLDER_PATH + "build/")) > 0:
+        print("current(under build folder) version will be tested!")
     else:
-        print("You can not continue without DynamoDB!")
+        print("can not be continued without build! Put telematics folder under the build directory!")
         exit()
-clear_dynamodb()
-# os.system("sh /Users/omerorhan/Documents/EventDetection/regression_server/regressiontest/build/telematics-server/server.sh stop")
-# os.system("sh /Users/omerorhan/Documents/EventDetection/regression_server/regressiontest/build/telematics-server/server.sh start")
-log_dataframe = upload_bin_batch_v2(FOLDER_PATH+"tripfiles/")
 
-trip_results = getTripsFromRegressionServer()
-combinedresult_s3key = pd.merge(log_dataframe, trip_results, on='trip_id')
-combinedresult_s3key.to_csv(FOLDER_PATH+"tripresults/trip_results.csv")
-compareTrips(FOLDER_PATH)
-print("Report is ready! Check repors folder!")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if sock.connect_ex(('localhost', 8000)) != 0:
+        print("activate DynamoDB! Please open new command prompt and run code in Readme.txt")
+        response = input("Is DynamoDB activated(Y/N)?")
+        if response.upper() != 'Y' and sock.connect_ex(('localhost', 8000)) != 0:
+            pass
+        else:
+            print("You can not continue without DynamoDB!")
+            exit()
+    clear_dynamodb()
+    # os.system("sh /Users/omerorhan/Documents/EventDetection/regression_server/regressiontest/build/telematics-server/server.sh stop")
+    # os.system("sh /Users/omerorhan/Documents/EventDetection/regression_server/regressiontest/build/telematics-server/server.sh start")
+    log_dataframe = upload_bin_batch_v2(FOLDER_PATH + "tripfiles/")
+
+    trip_results = getTripsFromRegressionServer()
+    combinedresult_s3key = pd.merge(log_dataframe, trip_results, on='trip_id')
+    combinedresult_s3key.to_csv(FOLDER_PATH + "tripresults/trip_results.csv")
+    compareTrips(FOLDER_PATH)
+    print("Report is ready! Check repors folder!")
+
+FOLDER_PATH=""
+if platform.node()=='dev-app-01-10-100-2-42.mentor.internal':
+    FOLDER_PATH="/home/ec2-user/regressiontest/"
+else:
+    FOLDER_PATH="/Users/omerorhan/Documents/EventDetection/regression_server/regressiontest/"
+
+test(FOLDER_PATH)
